6f147c3c227027b139952bc61396cb0c297e9898
NHutt28 <neil.hutton@menloschool.org>
2024-11-18 09:38:26 -0800

    Finish card class

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,43 @@
 public class Card {
+    // Instance Variables
+    private int value;
+    private String rank;
+    private String suit;
 
+    // Constructor
+    public Card(int value, String suit, String rank) {
+        this.value = value;
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    // Getter and Setter Methods
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    // ToString
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
 }
