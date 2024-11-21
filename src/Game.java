517918dd139019c8aff51265376873b025c47056
NHutt28 <neil.hutton@menloschool.org>
2024-11-21 13:43:39 -0800

    Finished dealing of War.

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,36 @@
+import java.util.Scanner;
+
 public class Game {
-
     private Deck a;
-    public Game(Deck deck) {
-        Deck a = deck;
-        System.out.println(a.getCards());
-    }
+    private Player p1;
+    private Player p2;
+    public Game() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is Player One's Name?");
+        String name = input.nextLine();
+        Player a = new Player(name);
+        System.out.println("What is Player Two's Name?");
+        name = input.nextLine();
+        Player b = new Player(name);
+        String[] ranks = {"Ace", "2", "3", "4","5", "6", "7", "8","9", "10", "Jack", "Queen", "King"};
+        String[] suits = {"Hearts", "Clubs", "Spades", "Diamonds"};
+        int[] values = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+        Deck deck = new Deck(values, ranks, suits);
+        for (int i = 0; i < 26; i++) {
+            a.addCard(deck.deal());
+            b.addCard(deck.deal());
+        }
+        System.out.println(a);
+        System.out.println(b);
 
+
+
+    }
     public Deck getDeck() {
         return a;
     }
 
     public static void main(String[] args) {
-        Player b = new Player("Neil");
-        String[] ranks = {"Ace", "2", "3", "4","5", "6", "7", "8","9", "10", "Jack", "Queen", "King"};
-        String[] suits = {"Hearts", "Clubs", "Spades", "Diamonds"};
-        int[] values = {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-        Deck deck = new Deck(values, ranks, suits);
-        Game x = new Game(deck);
-        b.addCard(deck.deal());
-        b.addCard(deck.deal());
-        System.out.println(b);
+        Game x = new Game();
     }
 }
