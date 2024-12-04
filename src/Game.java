b884aec72b678bab3c8558c3f099dfbaf7db8baf
NHutt28 <neil.hutton@menloschool.org>
2024-12-03 20:09:37 -0800

    Fixed entire game - it works now.

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class Game {
     public Game() {
         Scanner input = new Scanner(System.in);
+        String playAgain;
+        int infCount = 0;
         System.out.println("What is Player One's Name?");
         String name = input.nextLine();
         Player a = new Player(name);
@@ -14,7 +16,9 @@ public class Game {
         String[] suits = {"Hearts", "Clubs", "Spades", "Diamonds"};
         int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
         while(true) {
-
+            infCount = 0;
+            a.getHand().clear();
+            b.getHand().clear();
             Deck deck = new Deck(values, ranks, suits);
             for (int i = 0; i < 26; i++) {
                 a.addCard(deck.deal());
@@ -22,12 +26,11 @@ public class Game {
             }
             Card p1;
             Card p2;
-            Player winner;
-            ArrayList<Card> pile = new ArrayList<>();
-            while ((!a.getHand().isEmpty()) && (!b.getHand().isEmpty())) {
+            ArrayList<Card> pile = new ArrayList<Card>();
+            while (true) {
                 pile.clear();
-                p1 = a.hand.removeFirst();
-                p2 = b.hand.removeFirst();
+                p1 = a.hand.remove(0);
+                p2 = b.hand.remove(0);
 
                 pile.add(p1);
                 System.out.print(a.getName() + " played " + p1);
@@ -47,19 +50,27 @@ public class Game {
                         b.addCard(y);
                     }
                 }
+                infCount++;
+                if (infCount > 2000) {
+                    System.out.println("\n***************DRAW!***************");
+                    break;
+                } else if (a.getHand().isEmpty()) {
+                    b.addPoints(1);
+                    break;
+                } else if (b.getHand().isEmpty()) {
+                    a.addPoints(1);
+                    break;
+                }
             }
-            if(a.getHand().isEmpty())
+            System.out.println("\n<><><><><><><><><>\n" + a.getName() + " now has " + a.getPoints() + " win(s)!\n" + "<><><><><><><><><>\n");
+            System.out.println("<><><><><><><><><>\n" + b.getName() + " now has " + b.getPoints() + " win(s)!\n" + "<><><><><><><><><>\n");
+            System.out.println("Would you like to play again? (Type no if not)");
+            playAgain = input.nextLine();
+            if (playAgain.toLowerCase().equals("no"))
             {
-                winner = b;
-                b.addPoints(1);
+                break;
             }
-            else
-            {
-                winner = a;
-                a.addPoints(1);
-            }
-            System.out.println("\n<><><><><><><><><>\n" + winner.getName() + " now has " + winner.getPoints() + " win(s)!\n" + "<><><><><><><><><>\n");
-            break;
+
         }
     }
     public void war(ArrayList<Card> pile, Player a, Player b)
@@ -68,12 +79,12 @@ public class Game {
         Card w2;
         int lowerHand;
         int count = 0;
-        System.out.println("WAR!!");
         if (a.getHand().isEmpty() || b.getHand().isEmpty())
         {
             return;
         }
-        else if(a.getHand().size() <= 4 || b.getHand().size() <= 4)
+        System.out.println("WAR!!");
+        if(a.getHand().size() <= 4 || b.getHand().size() <= 4)
         {
             lowerHand = a.getHand().size();
             if(b.getHand().size() < lowerHand)
